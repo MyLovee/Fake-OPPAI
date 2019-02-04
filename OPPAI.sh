a3b71d5b9cb6041e32947192755bd9a7d818c70a
Labib/Mine7 <46355160+MyLovee@users.noreply.github.com>
2019-02-04 16:11:51 +0700

    Add files via upload

---
+++ OPPAI.sh
@@ -0,0 +1,100 @@
+clear
+#!/system/xbin/bash
+#ngapain lu bangsat
+#mau record?
+#apa faedah nya? biar di bilang mastah?
+#kok gua ngukuk ya
+#lu ngerecord gak bakal ngubah lu jadi mastah goblok
+#$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+#colors
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+indigo="\033[34;1m"
+clear
+echo "Asslamualaikum Wr Wb"
+sleep 1
+echo "Selamat Datang di Tolls Fake-OPPAI"
+sleep 2
+clear
+toilet -f future -F gay F a k e - O P P A I
+echo $yellow "_______________________________________"
+echo $cyan "Author : root@Mine7"
+echo $purple "Team : Nolep People Crew"
+echo $green "contact : 0895378276565"
+echo $yellow "——————————————————————————————————————"
+echo ""
+echo $white "1.Penyetabil Jaringan v1"
+echo $cyan "2.penyetabil Jaringan v2"
+echo $yellow "99.Info Tolls"
+echo $white "0.Keluar/Exit"
+echo ""
+echo $white"╭─root@Mine7~# "
+read -p "╰─$" kampang
+
+if [ $kampang = "0" ] || [ $kampang = "00" ]
+then
+echo $cyan
+clear
+figlet "keluar"
+echo $white "Terimakasi telah menggunakan tols kami semoga berfaedah :)"
+fi
+
+if [ $kampang = "99" ] || [ $kampang = "99" ]
+then
+clear
+toilet -f future -F gay Info Tolls
+echo""
+echo $yellow "Author : root@Mine7 / Labib         "
+echo $cyan   "Github : https://github.com/MyLovee "
+echo $yellow "Name Tolls : Fake-OPPAI"
+echo $cyan   "–=My Friends=–"
+echo $yellow "InYourHeart"
+echo $cyan   "Palkon"
+echo $yellow "Botak Nol3p"
+echo $cyan   "IY×KID"
+echo $yellow "Xlon3ly"
+echo $cyan   "IY×Labil"
+echo $yellow "Wahyu Dhimas/Ogeb"
+echo $cyan   "Tuan Ryu"
+echo $yellow "InYourGrzl"
+echo $green
+echo "╭─[B]:Back [E]:Exit "
+echo "|"$yellow
+read -p "╰─[B]/[E] :" kontol
+fi
+
+if [ $kontol = "B" ] || [ $kontol = "b" ]
+then
+clear
+sh OPPAI.sh
+fi
+
+if [ $kontol = "E" ] || [ $Kontol = "e" ]
+then
+clear
+echo $cyan
+clear
+figlet "keluar"
+echo $white "Terimakasi telah menggunakan tols kami semoga berfaedah :)"
+fi
+
+if [ $kampang = "1" ] || [ $kampang = "01" ]
+then
+clear
+toilet -f future -F gay Penyetabil JaringanV1
+echo $cyan ""
+ping 8.8.8.8
+fi
+
+if [ $kampang = "2" ] || [ $kampang = "02" ]
+then
+clear
+toilet -f future -F gay Penyetabil JaringanV2
+echo $cyan ""
+ping -D www.google.com
+fi
